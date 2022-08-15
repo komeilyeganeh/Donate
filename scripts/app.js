1582c6a3ae61b99e15974694d73ad442c2adf1d0
komeilyeganeh <komeilyeganeh261@gmail.com>
2022-08-15 21:04:24 +0430

    Edit login/register page

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,20 @@ btnTogglePass.addEventListener("click", () => {
         inputPass.setAttribute("type", "password");
     }
 });
+
+// ===== Switch between login and registration form
+let btnLogin = document.querySelector(".login-link a");
+let btnRegister = document.querySelector(".register-link a");
+let loginForm = document.querySelector(".login-content-box");
+let registerForm = document.querySelector(".register-content-box");
+
+btnLogin.addEventListener("click", (e) => {
+    e.preventDefault();
+    registerForm.classList.remove("active");
+    loginForm.classList.add("active");
+});
+btnRegister.addEventListener("click", (e) => {
+    e.preventDefault();
+    loginForm.classList.remove("active");
+    registerForm.classList.add("active");
+});
