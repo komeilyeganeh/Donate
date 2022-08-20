b4b5d72d332593c123f53256bfbb09b6f760abdf
komeilyeganeh <komeilyeganeh261@gmail.com>
2022-08-20 15:09:25 +0430

    Responsive service-donate page

--- scripts/app.js
+++ scripts/app.js
@@ -6,3 +6,16 @@ btnBars.addEventListener("click", () => {
     navbar.classList.toggle("active");
     btnBars.classList.toggle("active");
 });
+
+// ===== Toggle filter menu
+const btnToggleFilter = document.querySelector(".toggle-filter");
+const filter = document.querySelector(".donate-filter");
+const overlayBody = document.querySelector(".overlay-body");
+btnToggleFilter.addEventListener("click", () => {
+    filter.classList.toggle("active");
+    overlayBody.classList.toggle("active");
+});
+overlayBody.addEventListener("click", () => {
+    filter.classList.remove("active");
+    overlayBody.classList.remove("active");
+});
